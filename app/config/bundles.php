b15cece45a5be4ce2a7636409173324a8ee2d2cc
cooolinho <coding@cooolinho.de>
2020-08-14 16:27:20 +0200

    add user fixtures; login logik + template

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Cooolinho\UiBundle\CooolinhoUiBundle::class => ['all' => true],
     Cooolinho\SecurityBundle\CooolinhoSecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
